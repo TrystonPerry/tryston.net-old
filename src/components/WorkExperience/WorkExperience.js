a3f371a3bc39ebebf13d0d3821bfa78c5baf9736
Tryston Perry <trystonmperry@gmail.com>
2020-02-07 15:12:49 -0800

    Functionified work experience section

--- src/components/WorkExperience/WorkExperience.js
+++ src/components/WorkExperience/WorkExperience.js
@@ -1,43 +1,45 @@
 import React from 'react';
 import './WorkExperience.css';
 
+import data from "./../../content.js" 
+
 export default class Card extends React.Component {
   render() {
+    const { work } = data
+
+    function generateExperience() {
+      return (
+        work.map(item => {
+          return (
+            <li className="work-experience__list-item">
+              <div className="work-item__title">
+                <h3>{ item.company }</h3>
+                <span>{ item.title }</span>
+                <a
+                  href={ item.website }
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Website
+                </a>
+              </div>
+              <span className="work-item__time">
+                { item.startDate } - { item.endDate }
+              </span>
+              <ul className="work-item__duties">
+               { item.points.map(point => <li>{ point }</li>) }
+              </ul>
+            </li>
+          )
+        })
+      )
+    }
+
     return (
       <div className="work-experience card">
         <h2>Work Experience</h2>
         <ul className="work-experience__list">
-          <li className="work-experience__list-item">
-            <div className="work-item__title">
-              <h3>Nanome Inc.</h3>
-              <span>Front-End Developer</span>
-              <a
-                href="https://nanome.ai"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Website
-              </a>
-            </div>
-            <span className="work-item__time">March 2019 - January 2020</span>
-            <ul className="work-item__duties">
-              <li>
-                Built new landing page designs in Figma, went through
-                iterations, and implemented them in a Vue.js front-end.
-              </li>
-              <li>
-                Improved enterprise sales pipeline by designing new landing
-                pages from scratch https://home.nanome.ai/commercial.
-              </li>
-              <li>
-                Optimized content to improve web page load times by up to 500%.
-              </li>
-              <li>
-                Collaborated with co-workers in code review and pull requests.
-              </li>
-              <li>Stayed SEO and accessibility conscious.</li>
-            </ul>
-          </li>
+          { generateExperience() }
         </ul>
       </div>
     );
